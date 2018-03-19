ac2a24fb03fceb19cd8afa8a9691a3528e04493e
mtleinon <mika.t.leinonen@gmail.com>
2018-03-19 06:15:38 +0200

    Added bootstrap to jobs.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,6 +18,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.urls import include, paths
+import jobs.views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/',  admin.site.urls),
+    path('', jobs.views.home, name='home'),
+    #path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
